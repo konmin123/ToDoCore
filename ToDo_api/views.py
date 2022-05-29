6d2587a6c345a064db16c78d2e2b2f3dce181396
konmin123 <MindlinKS@yandex.ru>
2022-05-29 23:54:57 +0300

    add about.html

--- ToDo_api/views.py
+++ ToDo_api/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import render
+from django.views import View
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from ToDoCore import local_settings
 
 
 from ToDo_models.models import Task
 from . import serializers
 
+
+class AboutAPIView(View):
+    def get(self, request):
+        user = request.user
+        context = {
+            "server_version": local_settings.SERVER_VERSION, "user": user
+        }
+        return render(request, 'ToDo_api_templates/about.html', context=context)
 
 # class MyTasksListTodayAPIView(ListAPIView):
 #     queryset = Task.objects.all()
